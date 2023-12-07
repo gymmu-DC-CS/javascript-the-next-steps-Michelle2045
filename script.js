826816c5725e02e2c25f8f11d27711d02d8d15e3
Michii <michi.schreiberli@wegenstetten.com>
2023-12-07 15:42:08 +0100

    I still don't understand anything

--- script.js
+++ script.js
@@ -4,8 +4,53 @@ export function exercise01(args) {
 
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    if (currentElement!= "e" && currentElement!= "E"){
-    result.push(currentElement)
-  return result.join("")}
+    if (currentElement === "e" || currentElement === "E") {
+      result.push("")
     }
+    else {
+      result.push(currentElement)
     }
+  }
+  return result.join("")
+}
+
+
+export function exercise02 (args) {
+  return args toUpperCase()
+}
+
+export function exercise03 (args) {
+  const input =args
+  const result= []
+  let count= 0
+
+  for (let i = 0 ; i < input.length; i++){
+    const currentElement= input [i]
+    if (currentElement === 'e'){
+      count=count+1
+    }
+
+    else if (currentElement === 'E'){
+      count=count+1
+    }
+  }
+  return count
+}
+
+export function exercise04 (args) {
+  const input =args
+  const result= []
+  let count= 0
+
+  for (let i = 0 ; i < input.length; i++){
+    const currentElement= input [i]
+    if (currentElement === 'e'){
+      count=count+1
+    }
+
+    else if (currentElement === 'E'){
+      count=count+1
+    }
+  }
+  return count
+}
