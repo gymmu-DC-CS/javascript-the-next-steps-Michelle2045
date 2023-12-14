52a0d4a4465bb25c9a3cf5ddd9b90a46bf6a251d
Michii <michi.schreiberli@wegenstetten.com>
2023-12-14 15:17:49 +0100

    I had some problems with script.js, but I was able to fix them.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-export function exercise01(args) {
+export function exercise01(args) /*defining the function*/ {
   const input = args
   const result = []
 
@@ -6,50 +6,28 @@ export function exercise01(args) {
     const currentElement = input[i]
     if (currentElement === "e" || currentElement === "E") {
       result.push("")
-    }
-    else {
+    } else {
       result.push(currentElement)
     }
   }
   return result.join("")
 }
 
-
-export function exercise02 (args) {
-  return args toUpperCase()
+export function exercise02(args) {
+  return args.toUpperCase()
 }
 
-export function exercise03 (args) {
-  const input =args
-  const result= []
-  let count= 0
+export function exercise03(args) {
+  const input = args
+  let result = []
+  let count = 0
 
-  for (let i = 0 ; i < input.length; i++){
-    const currentElement= input [i]
-    if (currentElement === 'e'){
-      count=count+1
-    }
-
-    else if (currentElement === 'E'){
-      count=count+1
-    }
-  }
-  return count
-}
-
-export function exercise04 (args) {
-  const input =args
-  const result= []
-  let count= 0
-
-  for (let i = 0 ; i < input.length; i++){
-    const currentElement= input [i]
-    if (currentElement === 'e'){
-      count=count+1
-    }
-
-    else if (currentElement === 'E'){
-      count=count+1
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e" || currentElement === "E") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
     }
   }
   return count
