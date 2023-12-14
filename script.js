facf9c463c7350e3f23c8f5c04d9ad2ad0e65afa
Michii <michi.schreiberli@wegenstetten.com>
2023-12-14 15:36:52 +0100

    I tried exercise05 but i am not finish

--- script.js
+++ script.js
@@ -32,3 +32,39 @@ export function exercise03(args) {
   }
   return count
 }
+
+export function exercise04(args) {
+  const input = "Two Words"
+  const res = input
+  assert.equal(res, 2)
+  let result = []
+  let count = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "Two Words" || currentElement === " Two Words") {
+      count = spaces + 1
+    } else if (currentElement === " Two Words") {
+      count = spaces + 1
+    }
+  }
+  return count
+}
+
+export function exercise05(args) {
+  const input = "all lower case here...."
+  const res = funcs.exercise05(input)
+  assert.equal(res, false)
+  let result = []
+  let count = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "Two Words" || currentElement === " Two Words") {
+      count = spaces + 1
+    } else if (currentElement === " Two Words") {
+      count = spaces + 1
+    }
+  }
+  return count
+}
